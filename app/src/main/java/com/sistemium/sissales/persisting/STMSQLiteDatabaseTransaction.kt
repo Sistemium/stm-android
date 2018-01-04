6493b879f6879ffc77662521ada3bb8801b2dc13
Edgar Jan <krodanix@gmail.com>
2018-01-04 13:35:29 +0200

    major sql transaction fixes

--- app/src/main/java/com/sistemium/sissales/persisting/STMSQLiteDatabaseTransaction.kt
+++ app/src/main/java/com/sistemium/sissales/persisting/STMSQLiteDatabaseTransaction.kt
@@ -1,5 +1,7 @@
 package com.sistemium.sissales.persisting
 
+import android.database.Cursor.FIELD_TYPE_FLOAT
+import android.database.Cursor.FIELD_TYPE_INTEGER
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
 import com.sistemium.sissales.base.STMConstants
@@ -16,9 +18,9 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
 
     override fun findAllSync(entityName: String, predicate: STMPredicate?, options: Map<*, *>?): Array<Map<*, *>> {
 
-        val pageSize:Int? = options?.get(STMConstants.STMPersistingOptionPageSize) as? Int
-        var offset:Int? = options?.get(STMConstants.STMPersistingOptionStartPage) as? Int
-        val groupBy:Array<*>? = options?.get(STMConstants.STMPersistingOptionGroupBy) as? Array<*>?
+        val pageSize:Int? = (options?.get(STMConstants.STMPersistingOptionPageSize) as? Double)?.toInt()
+        var offset:Int? = (options?.get(STMConstants.STMPersistingOptionStartPage) as? Double)?.toInt()
+        val groupBy:Array<*>? = (options?.get(STMConstants.STMPersistingOptionGroupBy) as? ArrayList<*>)?.toArray()
 
         if (offset != null && pageSize != null) {
             offset -= 1
@@ -43,7 +45,6 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
 
         var options = ""
         var columns = ""
-        var _orderBy = orderBy
 
         var columnKeys = groupBy
 
@@ -53,7 +54,7 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
                 "[$it]"
             }.toTypedArray()
 
-            options = "GROUP BY " + columnKeys.joinToString(", ")
+            options = " GROUP BY " + columnKeys.joinToString(", ")
 
             columnKeys = columnKeys.plus(sumKeysForEntityName(entityName))
 
@@ -66,8 +67,8 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
         }
 
         if (orderBy != null) {
-            _orderBy = orderBy.split(",").joinToString( if (ascending) " ASC," else " DESC,")
-            val order = " ORDER BY $_orderBy ${if (ascending) " ASC," else " DESC,"}"
+            val _orderBy = orderBy.split(",").joinToString( if (ascending) " ASC," else " DESC,")
+            val order = " ORDER BY $_orderBy ${if (ascending) "ASC" else "DESC"}"
             options += order
         }
 
@@ -89,9 +90,11 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
             where = predicate.predicateForAdapter(adapter, entityName) ?: ""
             Log.d("DEBUG:EntityName", entityName)
             Log.d("DEBUG:PREDICATE", where)
+            Log.d("DEBUG:OPTIONS", options)
+            Log.d("DEBUG:COLUMNS", columns)
         }
 
-        return selectFrom(tableName, columns, where, _orderBy)
+        return selectFrom(tableName, columns, where, options)
 
     }
 
@@ -99,17 +102,30 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
 
         val numericTypes = arrayOf("Integer 16", "Integer 32", "Integer 64", "Decimal", "Double", "Float")
 
+        val minMaxTypes = arrayOf("Date")
+
         val tableColumns = adapter.model.fieldsForEntityName(entityName)
 
         var sumKeys = arrayOf<String>()
 
         tableColumns.map {
 
+            if (it.value.attributeName == "id"){
+
+                return@map
+
+            }
+
             val valueIsNumeric = numericTypes.contains(it.value.attributeType)
 
-            val keyIsIgnored = adapter.ignoredAttributeNames.contains(it.key)
+            val minMaxField = minMaxTypes.contains(it.value.attributeType)
 
-            if (valueIsNumeric && !keyIsIgnored) sumKeys = sumKeys.plus("sum([$sumKeys.plusAssign(it.value.attributeName)]) [sum($sumKeys.plusAssign(it.value.attributeName))]")
+            if (valueIsNumeric) {
+                sumKeys = sumKeys.plus("sum([${(it.value.attributeName)}]) [sum(${(it.value.attributeName)})]")
+            } else if (minMaxField || it.value.attributeName == "date") {
+                sumKeys = sumKeys.plus("max([${(it.value.attributeName)}]) [max(${(it.value.attributeName)})]")
+                sumKeys = sumKeys.plus("min([${(it.value.attributeName)}]) [min(${(it.value.attributeName)})]")
+            }
 
         }
 
@@ -129,28 +145,68 @@ class STMSQLiteDatabaseTransaction(private var database: SQLiteDatabase, private
             _where = " WHERE " + _where
         }
 
-        if (_orderBy != null) _orderBy = ""
+        if (_orderBy == null) _orderBy = ""
 
         val query = "SELECT $columns FROM [$tableName]$_where $_orderBy"
+
+        Log.d("DEBUG:QUERY", query)
 
         val c = this.database.rawQuery(query,null)
 
         if (c.moveToFirst()) {
             do {
 
-                val dict = hashMapOf<String, Any>()
+                val dict = hashMapOf<String, Any?>()
 
                 for (columnName in c.columnNames){
 
                     val index = c.getColumnIndex(columnName)
 
-                    val data = c.getString(index)
+                    val attributeType = adapter.model.entitiesByName.get(STMFunctions.addPrefixToEntityName(tableName))?.attributesByName?.get(columnName)?.attributeType
 
-                    if (data != null){
+                    if (c.isNull(index)){
 
-                        dict.put(columnName, data)
+                        dict.put(columnName, null)
+
+                        continue
 
                     }
+
+                    if (attributeType == "Boolean"){
+
+                        val data = c.getInt(index)
+
+                        if (data == 0){
+
+                            dict.put(columnName, false)
+
+                        }else{
+
+                            dict.put(columnName, true)
+
+                        }
+
+                        continue
+
+                    }
+
+                    if (c.getType(index) == FIELD_TYPE_INTEGER){
+
+                        dict.put(columnName, c.getInt(index))
+
+                        continue
+
+                    }
+
+                    if (c.getType(index) == FIELD_TYPE_FLOAT){
+
+                        dict.put(columnName, c.getDouble(index))
+
+                        continue
+
+                    }
+
+                    dict.put(columnName, c.getString(index))
 
                 }
 
