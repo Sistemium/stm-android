3445c54d095ccb24bc9c362ab4cb29321ecced9f
Edgar Jan <krodanix@gmail.com>
2018-01-29 18:45:01 +0200

    cashe modeler

--- app/src/main/java/com/sistemium/sissales/model/STMModeller.kt
+++ app/src/main/java/com/sistemium/sissales/model/STMModeller.kt
@@ -16,21 +16,38 @@ class STMModeller(modelJSON:String) : STMModelling {
 
     override val entitiesByName: Map<String, STMEntityDescription> = managedObjectModel.entitiesByName
 
+    private val _storageForEntityName = hashMapOf<String, STMStorageType>()
+
     override fun storageForEntityName(entityName: String): STMStorageType {
+
+        if (_storageForEntityName[entityName] != null){
+
+            return _storageForEntityName[entityName]!!
+
+        }
+
+        _storageForEntityName[entityName] = STMStorageType.STMStorageTypeNone
 
         val prefixedEntityName = STMFunctions.addPrefixToEntityName(entityName)
 
-        val entity = entitiesByName[prefixedEntityName] ?: return STMStorageType.STMStorageTypeNone
+        val entity = entitiesByName[prefixedEntityName] ?: return _storageForEntityName[entityName]!!
 
         val storeOption = entity.userInfo["STORE"]
 
-        if (entity.abstract) return STMStorageType.STMStorageTypeAbstract
+        if (entity.abstract) {
+            _storageForEntityName[entityName] = STMStorageType.STMStorageTypeAbstract
 
-        if (storeOption == null || storeOption == "FMDB" || storeOption == "STMSQLiteDatabaseOperation"){
-            return STMStorageType.STMStorageTypeSQLiteDatabase
+            return _storageForEntityName[entityName]!!
         }
 
-        return STMStorageType.STMStorageTypeNone
+        if (storeOption == null || storeOption == "FMDB" || storeOption == "STMSQLiteDatabaseOperation"){
+
+            _storageForEntityName[entityName] = STMStorageType.STMStorageTypeSQLiteDatabase
+
+            return _storageForEntityName[entityName]!!
+        }
+
+        return _storageForEntityName[entityName]!!
 
     }
 
@@ -42,33 +59,49 @@ class STMModeller(modelJSON:String) : STMModelling {
 
     }
 
+    private var _fieldsForEntityName = hashMapOf<String, Map<String, STMAttributeDescription>>()
+
     override fun fieldsForEntityName(entityName: String): Map<String, STMAttributeDescription> {
 
-        val _entityName = STMFunctions.addPrefixToEntityName(entityName)
+        if (_fieldsForEntityName[entityName] != null){
 
-        val current = entitiesByName[_entityName]?.attributesByName ?: hashMapOf()
-
-        if (entitiesByName[_entityName]?.parentEntity != null){
-            return current.plus(fieldsForEntityName(entitiesByName[_entityName]?.parentEntity!!))
+            return _fieldsForEntityName[entityName]!!
 
         }
 
-        return current
+        val _entityName = STMFunctions.addPrefixToEntityName(entityName)
+
+        _fieldsForEntityName[entityName] = entitiesByName[_entityName]?.attributesByName ?: hashMapOf()
+
+        if (entitiesByName[_entityName]?.parentEntity != null){
+            _fieldsForEntityName[entityName] = _fieldsForEntityName[entityName]!!.plus(fieldsForEntityName(entitiesByName[_entityName]?.parentEntity!!))
+
+        }
+
+        return _fieldsForEntityName[entityName]!!
 
     }
 
+    private var _objectRelationshipsForEntityName = hashMapOf<String, Map<String, STMRelationshipDescription>>()
+
     override fun objectRelationshipsForEntityName(entityName: String, isToMany: Boolean?, cascade: Boolean?, optional: Boolean?): Map<String, STMRelationshipDescription> {
 
-        val _entityName = STMFunctions.addPrefixToEntityName(entityName)
+        if (_objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"] != null){
 
-        val current = entitiesByName[_entityName]?.relationshipsByName ?: hashMapOf()
-
-        if (entitiesByName[_entityName]?.parentEntity != null){
-            return current.plus(objectRelationshipsForEntityName(entitiesByName[_entityName]?.parentEntity!!))
+            return _objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"]!!
 
         }
 
-        return current
+        val _entityName = STMFunctions.addPrefixToEntityName(entityName)
+
+        _objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"] = entitiesByName[_entityName]?.relationshipsByName ?: hashMapOf()
+
+        if (entitiesByName[_entityName]?.parentEntity != null){
+            _objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"] = _objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"]!!.plus(objectRelationshipsForEntityName(entitiesByName[_entityName]?.parentEntity!!))
+
+        }
+
+        return _objectRelationshipsForEntityName["$entityName$isToMany$cascade$optional"]!!
 
     }
 
