d03585eab6e2a131ae3a4ef07a76cb468e18a356
Edgar Jan <EdgarJan@users.noreply.github.com>
2020-08-26 18:59:04 +0300

    TabBarControllerActivity

---
+++ app/src/main/java/com/sistemium/sissales/activities/TabBarControllerActivity.kt
@@ -0,0 +1,14 @@
+package com.sistemium.sissales.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TabBarControllerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
