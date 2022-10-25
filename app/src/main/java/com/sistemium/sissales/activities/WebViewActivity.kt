facf7713f715b176014f6fd022d2bd6566c167ad
Edgar Jan <EdgarJan@users.noreply.github.com>
2022-10-25 18:10:54 +0300

    flutter module

--- app/src/main/java/com/sistemium/sissales/activities/WebViewActivity.kt
+++ app/src/main/java/com/sistemium/sissales/activities/WebViewActivity.kt
@@ -293,7 +293,6 @@ class WebViewActivity : Activity() {
 
     private fun netDirectLoad(url: String) {
 
-        webView!!.settings!!.setAppCacheEnabled(false)
         webView!!.settings!!.cacheMode = WebSettings.LOAD_NO_CACHE
         webView?.settings?.userAgentString = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
 
