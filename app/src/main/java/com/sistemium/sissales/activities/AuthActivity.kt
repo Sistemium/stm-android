d7ba4fce763aeaed561071c36480f975c0e49b58
Edgar Jan <EdgarJan@users.noreply.github.com>
2022-12-23 17:37:01 +0200

    vfs client logo

--- app/src/main/java/com/sistemium/sissales/activities/AuthActivity.kt
+++ app/src/main/java/com/sistemium/sissales/activities/AuthActivity.kt
@@ -1,9 +1,4 @@
 package com.sistemium.sissales.activities
-import androidx.annotation.NonNull
-import com.sistemium.sissales.base.MyApplication
-import com.sistemium.sissales.base.classes.entitycontrollers.STMCorePicturesController
-import com.sistemium.sissales.base.session.STMCoreAuthController
-import com.sistemium.sissales.base.session.STMSession
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
